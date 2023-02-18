954b32bcf8c7cd59f719ea706051f669dbd8701f
Camilo-J <josecamilo1902@outlook.com>
2023-02-17 20:50:34 -0500

    Refactor styles in imageCard

--- src/components/imageCard.jsx
+++ src/components/imageCard.jsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 
 function ImageCard({ image, github, demo, title, content }) {
   return (
-    <div className="group/images basis-1/3 flex-1 relative overflow-hidden ">
+    <div className="group/images relative overflow-hidden max-w-lg ">
       <Image
         src={image}
-        alt=""
-        className="rounded-lg object-cover group-hover/images:blur-sm  "
+        alt="project"
+        className="rounded-lg object-cover group-hover/images:blur-sm h-full"
         width={"100%"}
-        height={100}
+        height="100"
         layout="responsive"
       />
-      <div className="group/edit top-full p-4 flex flex-col justify-center sm:gap-8 absolute text-white  h-full w-full  bg-blue-400/60 group-hover/images:top-0  transition-all duration-700">
-        <div className="flex flex-col gap-2 sm:gap-8 text-center">
+      <div className="group/edit top-full p-4 flex flex-col justify-center gap-2 sm:gap-8 absolute text-white  h-full w-full  bg-blue-400/60 group-hover/images:top-0  transition-all duration-700">
+        <div className="flex flex-col gap-1 sm:gap-8 text-center">
           <h4 className="text-4xl font-semibold">{title}</h4>
           <p className="text-lg font-medium">{content}</p>
         </div>
