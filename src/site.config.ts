b3b8ffe9e3fc6e315e3114f650960fcf2cbf0005
Camilo-J <josecamilo1902@outlook.com>
2025-11-29 12:51:03 -0500

    feat: add linter and assist overrides for Svelte, Astro, and Vue files; update portfolio title

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '@/types/site';
 
 export const siteConfig: SiteConfig = {
   author: 'Camilo Huanca',
-  title: '',
+  title: 'Camilo Huanca - Portfolio',
   description: "Camilo Huanca's resume and portfolio",
   lang: 'en-GB',
   ogLocale: 'en_GB',
